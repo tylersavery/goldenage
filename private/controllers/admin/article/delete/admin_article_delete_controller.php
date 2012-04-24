aa0c5176d5d8b2daeb9183e06d2ce47a6689a7d0
Tyler Savery <tyler@dinosaur-factory.com>
2012-04-24 16:18:06 -0700

    quick update

--- private/controllers/admin/article/delete/admin_article_delete_controller.php
+++ private/controllers/admin/article/delete/admin_article_delete_controller.php
@@ -4,14 +4,17 @@ class Admin_Article_Delete_Controller extends Admin_Article_Controller {
     function __construct($uri, $data) {
         parent::__construct($uri, $data);
         $this->title .= ' | Delete';
-    }
-    
-    protected function content_view() {
+        
         if (isset($this->data['article_id'])) {
             $article = Article_Model::find_by_id($this->data['article_id']);
             $article->delete();
         }
         redirect_to('/admin/articles');
+        
+    }
+    
+    protected function content_view() {
+        
     }
     
 }
