53a8348311bdced674bf6e373808387ed19fb53c
Tyler Savery <tyler@dinosaur-factory.com>
2012-04-24 19:10:56 -0700

    quick update

--- private/views/main_footer_view.php
+++ private/views/main_footer_view.php
@@ -3,7 +3,7 @@
         <div class="divider"></div>
         <div id="footer_left">&copy; 2012 The Golden Age</div>
         <div id="footer_right">
-        <a href="/">Archive</a> | <a href="/feed/rss">RSS</a> | 
+        <a href="/archive">Archive</a> | <a href="/feed/rss">RSS</a> | 
         <a href="https://www.facebook.com/pages/Golden-Age/246192312122135">Facebook</a> | <a href="https://twitter.com/#!/goldenagemgmt">Twitter</a>
         
         </div>
